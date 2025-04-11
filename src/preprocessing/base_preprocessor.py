06a119b51ebaa83a69bcf1ac90c5cc8c4f5a0d13
nikos.bentis <nikos.bentis@deptagency.com>
2025-04-11 17:23:00 +0200

    Base preprossor class

---
+++ src/preprocessing/base_preprocessor.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from src.utils.helpers import load_config
+
+
+class BasePreprocessor:
+    def __init__(self, config_path: str = "config/preprocessor_config.yaml"):
+        """
+        Load configuration from the given YAML file and set up common parameters.
+        """
+        config = load_config(config_path)
+        self.drop_cols = config.get("drop_columns", [])
+
+    def transform(self, raw_data: dict) -> pd.DataFrame:
+        """
+        Convert a raw input (dictionary) to a pandas DataFrame and drop columns specified in the config.
+        """
+        df = pd.DataFrame([raw_data])
+        df = df.drop(columns=self.drop_cols, errors="ignore")
+        return df
